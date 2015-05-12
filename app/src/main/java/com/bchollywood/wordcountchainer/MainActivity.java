3755b115a451bb4ed04fc1af099538e601bc33dd
BarryHoll <bchollgithub@gmail.com>
2015-05-12 19:39:57 +0100

    Adding simple button and text view. Button to be replaced with Photoshop magic in the future...

--- app/src/main/java/com/bchollywood/wordcountchainer/MainActivity.java
+++ app/src/main/java/com/bchollywood/wordcountchainer/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +15,19 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button bigButton = (Button) findViewById(R.id.big_button);
+        final TextView congratsText = (TextView) findViewById(R.id.congrats_text);
+        congratsText.setVisibility(View.INVISIBLE);
+
+        bigButton.setOnClickListener(
+                new Button.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        congratsText.setVisibility(View.VISIBLE);
+                    }
+                }
+        );
     }
 
     @Override
