8e8d68584f5fd2ac0a47bd27ceaad909f6c3af96
BarryHoll <bchollgithub@gmail.com>
2015-05-14 20:01:35 +0100

    Refactoring WordCountEntry

--- app/src/main/java/com/bchollywood/wordcountchainer/WordCountEntry.java
+++ app/src/main/java/com/bchollywood/wordcountchainer/WordCountEntry.java
@@ -1,17 +1,40 @@
 package com.bchollywood.wordcountchainer;
 
+import java.util.Date;
+
 /**
  * Created by Barry on 13/05/2015.
  */
 public class WordCountEntry {
     private int wordCount;
-    private String datePlaceHolder;
-    private String timePlaceHolder;
+    private Date todaysDate;
 
-    public WordCountEntry(int wordCount, String datePlaceHolder, String timePlaceHolder) {
+    public WordCountEntry(int wordCount, Date todaysDate) {
         this.wordCount = wordCount;
-        this.datePlaceHolder = datePlaceHolder;
-        this.timePlaceHolder = timePlaceHolder;
+        this.todaysDate = todaysDate;
     }
 
+    public WordCountEntry(int wordCount) {
+        this.todaysDate = new Date();
+    }
+
+    public WordCountEntry() {
+
+    }
+
+    public int getWordCount() {
+        return wordCount;
+    }
+
+    public void setWordCount(int wordCount) {
+        this.wordCount = wordCount;
+    }
+
+    public Date getTodaysDate() {
+        return todaysDate;
+    }
+
+    public void setTodaysDate(Date todaysDate) {
+        this.todaysDate = todaysDate;
+    }
 }
